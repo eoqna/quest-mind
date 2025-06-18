91d203295aeadf79f1bfbb8b4c9484423f0c2a30
eoqna <eoqna@naver.com>
2025-06-18 22:38:42 +0900

    인스타그램 게시글 클론 코딩

--- lib/main.dart
+++ lib/main.dart
@@ -12,47 +12,168 @@ class MyApp extends StatelessWidget {
     return MaterialApp(
       home: Scaffold(
         appBar: AppBar(
-          title: Text('금호동3가'),
-          titleTextStyle: TextStyle(
-            fontWeight: FontWeight.w700,
-          ),
+          title: Text("instagram"),
           actions: [
-            Icon(Icons.search),
-            Icon(Icons.menu),
-            Icon(Icons.notifications_none_outlined)
+            Icon(Icons.favorite_outline),
+            Icon(Icons.send_outlined),
           ],
         ),
-        body: Container(
-          height: 150,
-          padding: EdgeInsets.all(10),
-          child: Row(
-            mainAxisAlignment: MainAxisAlignment.start,
-            crossAxisAlignment: CrossAxisAlignment.start,
-            children: [
-              Image.asset('mac.png', width: 150),
-              Container(
-                padding: EdgeInsets.fromLTRB(10, 0, 10, 0),
-                width: 300,
-                child: Column(
-                  crossAxisAlignment: CrossAxisAlignment.start,
-                  children: [
-                    Text('캐논 DSLR 100D (단렌즈, 충전기 16기가SD 포함'),
-                    Text('성수동 행당동 ・ 끌올 10분 전'),
-                    Text('210,000원'),
-                    Row(
-                      mainAxisAlignment: MainAxisAlignment.end,
-                      children: [
-                        Icon(Icons.favorite_outline),
-                        Text('4'),
-                      ],
-                    )
-                  ],
-                ),
-              )
-            ],
-          )
-        )
+        body: BodyContainer(),
+        bottomNavigationBar: AppBarContainer(),
       )
+    );
+  }
+}
+
+class BodyContainer extends StatelessWidget {
+  const BodyContainer({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return ListView(
+      children: [
+        PostItem(),
+        PostItem(),
+        PostItem(),
+        PostItem(),
+      ],
+    );
+  }
+}
+
+class PostItem extends StatelessWidget {
+  const PostItem({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return SizedBox(
+      width: double.infinity,
+      child: Column(
+        children: [
+          AccountInfoInPost(),
+          ImageContainerInPost(),
+          ButtonItemInPost(),
+          TextBoxInPost(),
+        ],
+      ),
+    );
+  }
+}
+
+class AccountInfoInPost extends StatelessWidget {
+  const AccountInfoInPost({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      padding: EdgeInsetsGeometry.fromLTRB(10, 15, 10, 0),
+      child: Row(
+        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+        children: [
+          Row(
+            children: [
+              Icon(Icons.account_circle_outlined),
+              Text("tlseoqja")
+            ],
+          ),
+          Icon(Icons.more_horiz),
+        ],
+      ),
+    );
+  }
+}
+
+class ImageContainerInPost extends StatelessWidget {
+  const ImageContainerInPost({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      padding: EdgeInsetsGeometry.fromLTRB(0, 10, 0, 5),
+      child: Image.asset(
+        "instagram.png",
+        width: double.infinity,
+        fit: BoxFit.fitWidth,
+      ),
+    );
+  }
+}
+
+
+class ButtonItemInPost extends StatelessWidget {
+  const ButtonItemInPost({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      padding: EdgeInsetsGeometry.fromLTRB(10, 5, 10, 5),
+      child: Row(
+        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+        children: [
+          Flexible(
+            flex: 7,
+            child: Row(
+              children: [
+                Icon(Icons.favorite_outline),
+                Text("250"),
+                Icon(Icons.message_outlined),
+                Text("39"),
+                Icon(Icons.send_outlined),
+                Text("390"),
+                Icon(Icons.refresh_outlined),
+                Text("4"),
+              ],
+            ),
+          ),
+          Flexible(
+            flex: 3,
+            child: Icon(Icons.save)
+          ),
+        ],
+      ),
+    );
+  }
+}
+
+class TextBoxInPost extends StatelessWidget {
+  const TextBoxInPost({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      padding: EdgeInsetsGeometry.fromLTRB(10, 5, 10, 5),
+      child: Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: [
+          Row(
+            children: [
+              Text("tlseoqja", strutStyle: StrutStyle(fontWeight: FontWeight.bold)),
+              Text("인스타그램 시작한다ㅋ"),
+            ],
+          ),
+          Text("4시간 전")
+        ],
+      ),
+    );
+  }
+}
+
+class AppBarContainer extends StatelessWidget {
+  const AppBarContainer({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return BottomAppBar(
+      child: Row(
+        mainAxisAlignment: MainAxisAlignment.spaceAround,
+        children: [
+          Icon(Icons.home),
+          Icon(Icons.search),
+          Icon(Icons.add_box_outlined),
+          Icon(Icons.movie_creation_outlined),
+          Icon(Icons.account_circle),
+        ],
+      ),
     );
   }
 }
